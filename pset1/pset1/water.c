fd5f9deef107b67c67143f26951070c3e6b092c9
taketa <853211b@gmail.com>
2016-09-12 12:19:23 +0300

    pset1

--- pset1/pset1/water.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <cs50.h>
-int main(void){
-    printf("minutes: ");
-    int minutes = get_int();
-    int bottles = 12 * minutes;
-    printf("bottles: %i \n", bottles);
-    return 0;
-}
