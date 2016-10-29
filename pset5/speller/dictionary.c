46587adb81b12e0b18dc84e51a672c1d1995c2ea
taketa <853211b@gmail.com>
2016-10-29 21:05:26 +0300

    pset5

---
+++ pset5/speller/dictionary.c
@@ -0,0 +1,110 @@
+/**
+* Implements a dictionary's functionality.
+*/
+
+#include <stdbool.h>
+
+#include "dictionary.h"
+
+#include <strings.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+
+
+
+int hash(const char* word);
+const int HASHTABLE = 26;
+int count = 0;
+char word[LENGTH + 1];
+
+struct node{
+    char *word;
+    struct node* next;
+};
+
+struct node *hashTable[HASHTABLE];
+
+/**
+ * Returns true if word is in dictionary else false.
+ */
+bool check(const char* word)
+{
+    struct node* wordFromDictionary = malloc(sizeof(struct node));
+    int bucket = hash(word);
+    wordFromDictionary = hashTable[bucket];
+    while (wordFromDictionary != NULL){
+        // use strcasecmp to be case insensitive
+        if (strcasecmp(wordFromDictionary->word, word) == 0)
+            return true;
+        wordFromDictionary = wordFromDictionary->next;
+    }
+    return false;
+}
+
+/**
+ * Loads dictionary into memory.  Returns true if successful else false.
+ */
+bool load(const char* dictionary)
+{
+    FILE* dict = fopen(dictionary, "r");
+    if (dict == NULL)
+        return false;
+    while (fscanf(dict, "%s\n", word) != EOF){
+        struct node *new = malloc(sizeof(struct node));
+        new->word = malloc(strlen(word));
+
+        // copy word into pointer
+        strcpy(new->word, word);
+        int hashValue = hash(word);
+
+        // if new belongs at head, prepend
+        if (hashTable[hashValue] == NULL){
+            new->next = NULL;
+            hashTable[hashValue] = new;
+        }
+        else{
+            new->next = hashTable[hashValue];
+            hashTable[hashValue] = new;
+        }
+        count++;
+    }
+    fclose(dict);
+    return true;
+}
+
+int hash(const char* word)
+{
+    int index = tolower(word[0]);
+    return index % HASHTABLE;
+}
+
+/**
+ * Returns number of words in dictionary if loaded else 0 if not yet loaded.
+ */
+unsigned int size(void)
+{
+    // TODO
+    return count;
+}
+
+/**
+ * Unloads dictionary from memory.  Returns true if successful else false.
+ */
+bool unload(void)
+{
+    // TODO
+
+    for (int i = 0; i < HASHTABLE; i++){
+        struct node *cursor = hashTable[i];
+        while (cursor != NULL){
+            struct node* temp = cursor;
+            cursor = cursor->next;
+            free(temp);
+            return true;
+        }
+    }
+    return false;
+}
